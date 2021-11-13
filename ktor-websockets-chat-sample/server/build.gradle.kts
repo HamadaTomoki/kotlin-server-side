5816cef28f2a49d4568067c4cf6c0e6d731810d1
HamadaTomoki <hamada.tomoki01@gmail.com>
2021-11-14 03:42:32 +0900

    [Update] WebSocketのサーバー側を実装

--- ktor-websockets-chat-sample/server/build.gradle.kts
+++ ktor-websockets-chat-sample/server/build.gradle.kts
@@ -13,7 +13,12 @@ val ktor_version: String by project
 val logback_version: String by project
 
 dependencies {
+    // ktor-server-nettyはKtorをNettyエンジンと一緒に追加し、外部のアプリケーションコンテナに依存することなくサーバー機能を使用できるようにします。
     implementation("io.ktor:ktor-server-netty:$ktor_version")
+
+    // ktor-websocketsを使用すると、チャットの主要な通信メカニズムであるWebSocketKtorプラグインを使用できます。
     implementation("io.ktor:ktor-websockets:$ktor_version")
+
+    // logback-classicはSLF4Jの実装を提供し、コンソールで適切にフォーマットされたログを表示できるようにします。
     implementation("ch.qos.logback:logback-classic:$logback_version")
 }
--- ktor-websockets-chat-sample/server/src/main/kotlin/com/jetbrains/handson/chat/server/Application.kt
+++ ktor-websockets-chat-sample/server/src/main/kotlin/com/jetbrains/handson/chat/server/Application.kt
@@ -11,5 +11,21 @@ fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused")
 fun Application.module() {
+    /** WebSockets Ktorプラグインをインストールして、Ktorフレームワークによって提供されるWebSocket関連の機能を有効化 */
+    install(WebSockets)
+
+    /** WebSocketプロトコルに応答するエンドポイントをルーティングで定義 */
+    routing {
+        webSocket("/chat") {
+            send("You are connected!")
+            for (frame in incoming) {
+                // 着信チャネルを反復処理するときに受信する非テキストフレームはスキップ
+                frame as? Frame.Text ?: continue
+                // 受信したテキストを読み取り、「Yousaid：」というプレフィックスを付けてユーザーに送り返します。
+                val receivedText = frame.readText()
+                send("You said: $receivedText")
+            }
+        }
+    }
 
 }
